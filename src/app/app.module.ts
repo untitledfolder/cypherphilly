a483b7eadf410882896b1ee28baf713b10c078f0
Jason Cox <jasonwilburcox@gmail.com>
2018-02-26 00:05:31 +0000

    Adding a Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
