1e4c140c9e58bf0d269fb06130768b01dae1d86c
Jason Cox <jasonwilburcox@gmail.com>
2018-04-24 22:15:44 +0000

    Restructured scrapers

--- scrapers/run-all.sh
+++ scrapers/run-all.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-SCRAPERS="./scrapers"
+SCRAPERS="./"
 
-if [ -n "$1" ]; then
+if [[ -n "$1" ]] && [[ "-d" == "$1" ]] && [[ -n "$2" ]]; then
+  shift
   SCRAPERS="$1"
   shift
 fi
 
 for scraper in `find $SCRAPERS -type f -iname "*-scraper.sh"`; do
-  echo "Scraper: $scraper"
+  $scraper $@
 done
