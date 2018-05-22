3053e47697c8d77f21b88fdac6074b10d43a3fc0
Jason Cox <jasonwilburcox@gmail.com>
2018-05-22 13:31:11 +0000

    Run getter or tests

--- scrapers/run-all.js
+++ scrapers/run-all.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var exec = require('child_process').exec;
-var test = false;
+var test = true;
 var help = false;
 var dataSources = "./scrapers";
 var args = Object.assign([], process.argv);
@@ -28,6 +28,47 @@ console.log("Help:", help);
 console.log("Test:", test);
 console.log("Sources:", dataSources);
 
+function processDataSetConfig(dataConfig) {
+  if (dataConfig.length) {
+    console.log("Data Config:", dataConfig);
+    // TODO: Only do if it doesn't start with one of these: ['./', '../', '/']
+    var config = require('../' + dataConfig);
+
+    console.log("\tData Group Name:", config.datagroup.dataGroupName);
+    config.datagroup.datasets.forEach(function (dataset) {
+      console.log("\t\tData Set Name:", dataset.name);
+      var getDataCommand;
+      if (test && dataset.test && dataset.test.file) {
+        console.log("\t\tTest Data:", dataset.test.file);
+        getDataCommand = "cat " + dataset.test.file;
+      }
+      else if (!test) {
+        console.log("\t\tSource:", dataset.source);
+        getDataCommand = "wget " + dataset.source;
+      }
+      else {
+        console.log("\t\tNo tests");
+        return;
+      }
+
+      processDataSet(getDataCommand, dataset.processor);
+    });
+  }
+}
+
+function processDataSet(getDataCommand, processDataScript) {
+  exec(
+    getDataCommand + ' | ' + processDataScript,
+    function(error, stdout, stderr) {
+      if (error) {
+        console.log("Error:", error);
+      }
+
+      process.stdout.write(stdout);
+    }
+  );
+}
+
 console.log("Starting Scrapers");
 exec(
   'find ' + dataSources + ' -type f -iname "*-config.js"',
@@ -37,26 +78,6 @@ exec(
       process.exit(1);
     }
 
-    stdout.split(/\r?\n/).forEach(function(dataConfig) {
-      if (dataConfig.length) {
-        console.log("Data Config:", dataConfig);
-        // TODO: Only do if it doesn't start with one of these: ['./', '../', '/']
-        var config = require('../' + dataConfig);
-
-        console.log("\tData Group Name:", config.datagroup.dataGroupName);
-        config.datagroup.datasets.forEach(function (dataset) {
-          console.log("\t\tData Set Name:", dataset.name);
-          if (test && dataset.test && dataset.test.file) {
-            console.log("\t\tTest Data:", dataset.test.file);
-          }
-          else if (!test) {
-            console.log("\t\tSource:", dataset.source);
-          }
-          else {
-            console.log("\t\tNo tests");
-          }
-        });
-      }
-    });
+    stdout.split(/\r?\n/).forEach(processDataSetConfig);
   }
 );
