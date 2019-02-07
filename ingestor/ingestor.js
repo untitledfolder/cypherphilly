528a98b47ee3c29dd06a3286fafc97bd17ff51eb
Jason Cox <jasonwilburcox@gmail.com>
2019-02-07 22:26:13 +0000

    Params

--- ingestor/ingestor.js
+++ ingestor/ingestor.js
@@ -1,3 +1,11 @@
 #!/usr/local/bin/node
 
-console.log("INGESTOR");
+if (process.argv.length !== 3) {
+  console.log("Must specify what to ingest");
+  console.log(process.argv);
+  process.exit(1);
+}
+
+var ingestorConfig = process.argv[2];
+
+console.log("INGESTOR:", ingestorConfig);
