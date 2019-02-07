d9d34f39c320e681f262fad276237b582ac85599
Jason Cox <jasonwilburcox@gmail.com>
2019-02-07 22:41:20 +0000

    Debugs, since it's closer to right

--- ingest.sh
+++ ingest.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-INGESTOR_CONFIGS_DIR="./ingestor/datasets"
+DEBUG_INGESTOR=false
+
+WORKING_DIR="$(dirname $BASH_SOURCE[0])"
+INGESTOR_CONFIGS_DIR="$WORKING_DIR/ingestor/datasets"
 
 echo "Ingestor"
 echo
+
+function debug_message {
+  if [[ $DEBUG_INGESTOR == true ]]; then
+    echo "DEBUG: $@"
+  fi
+}
+debug_message "Working dir: $WORKING_DIR"
 
 INGEST_LIST="$@"
 if [ -z "$INGEST_LIST" ]; then
   INGEST_LIST="*RUN ALL*"
 fi
 
-echo "Ingest list: $INGEST_LIST"
+debug_message "Ingest list: $INGEST_LIST"
 for ingest_item in "$INGEST_LIST"; do
   if [ ! -f "$INGESTOR_CONFIGS_DIR/$ingest_item/config.json" ]; then
     echo "No dataset config for $ingest_item"
     continue
   fi
 
-  echo "START INGEST: $ingest_item"
-  ./ingestor/ingestor.js $INGESTOR_CONFIGS_DIR/$ingest_item/config.json
-  echo "END INGEST: $ingest_item"
+  debug_message "START INGEST: $ingest_item"
+  debug_message
+  $WORKING_DIR/ingestor/ingestor.js $INGESTOR_CONFIGS_DIR/$ingest_item/config.json
+  debug_message
+  debug_message "END INGEST: $ingest_item"
 done
