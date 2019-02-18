6ae0700349b33d8c35b3391f08c3b4b171323d6a
Jason Cox <jasonwilburcox@gmail.com>
2019-02-18 04:55:14 +0000

    Slight changes

--- ingestor/ingestor.js
+++ ingestor/ingestor.js
@@ -3,7 +3,11 @@
 var fs = require("fs");
 var prettyjson = require("prettyjson");
 
-if (process.argv.length !== 4) {
+var workingDir = __dirname;
+console.log("Working dir:", workingDir);
+var util = require(workingDir + "/ingest-util");
+
+if (process.argv.length < 4) {
   console.log("Must specify what to ingest");
   console.log(process.argv);
   process.exit(1);
@@ -13,10 +17,6 @@ var ingestorConfigKey = process.argv[2];
 var ingestorConfigFile = process.argv[3];
 
 console.log("~~~ INGESTOR ~~~");
-
-var workingDir = __dirname;
-console.log("Working dir:", workingDir);
-var util = require(workingDir + "/ingest-util");
 
 var ingestorConfig = JSON.parse(fs.readFileSync(ingestorConfigFile));
 console.log();
