cb096a3a45b1975f838668673063557a9c73f049
Jason Cox <jasonwilburcox@gmail.com>
2019-03-18 16:09:49 +0000

    Getting info from code instead of hardcode

--- src/app/components/image-content.component.ts
+++ src/app/components/image-content.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./image-content.component.scss']
 })
 
-export class ImageContentComponent { }
+export class ImageContentComponent {
+
+  title: string;
+  text: string;
+  imageUrl: string;
+
+  constructor() {
+    this.title = "TITLE!";
+    this.text = "TEXT!";
+    this.imageUrl = "https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg";
+  }
+
+}
