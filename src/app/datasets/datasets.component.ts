a968cb5fd6af568694af51af862993a09fd510f7
Jason Cox <jasonwilburcox@gmail.com>
2019-03-25 17:23:56 +0000

    Making room for new datasets page

---
+++ src/app/datasets/datasets.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'datasets',
+  templateUrl: './datasets.component.html',
+  styleUrls: ['./datasets.component.scss']
+})
+
+export class DatasetsComponent {
+
+  constructor() {}
+
+}
