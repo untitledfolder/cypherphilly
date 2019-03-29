b9b313bc0528a7bfd0138a708409faef60f3cee0
Jason Cox <jasonwilburcox@gmail.com>
2019-03-29 19:58:40 +0000

    Test the output

--- test/ingestor/ingestor-reader-test.js
+++ test/ingestor/ingestor-reader-test.js
@@ -63,9 +63,107 @@ describe.only('Reader', () => {
     });
   });
 
-  xdescribe('IO', () => {
-    xit('should generate io', () => {
-      assert.fail('not implemented');
+  describe('IO', () => {
+    it('should output csv as objects', (done) => {
+      var obj1;
+      var obj2;
+      var obj3;
+      var obj4;
+
+      reader = util.reader.new('csv', fixturesDir + 'example.csv');
+
+      reader.on('data', data => {
+        if (!obj1) obj1 = data;
+        else if (!obj2) obj2 = data;
+        else if (!obj3) obj3 = data;
+        else obj4 = data;
+      });
+
+      reader.on('end', () => {
+        expect(obj1).to.deep.equal({
+          cap_number: "13-0001",
+          date_received: "2013-01-07T00:00:00Z",
+          dist_occurrence: "35",
+          general_cap_classification: "BAD",
+          summary: "Summary 1",
+          the_geom: "null",
+          the_geom_webmercator: "null",
+          cartodb_id: "1"
+        });
+        expect(obj2).to.deep.equal({
+          cap_number: "13-0002",
+          date_received: "2013-01-09T00:00:00Z",
+          dist_occurrence: "35",
+          general_cap_classification: "GOOD",
+          summary: "Summary 2",
+          the_geom: "null",
+          the_geom_webmercator: "null",
+          cartodb_id: "2"
+        });
+        expect(obj3).to.deep.equal({
+          cap_number: "13-0003",
+          date_received: "2013-02-07T00:00:00Z",
+          dist_occurrence: "35",
+          general_cap_classification: "SOSO",
+          summary: "Summary 3",
+          the_geom: "null",
+          the_geom_webmercator: "null",
+          cartodb_id: "3"
+        });
+        expect(obj4).to.be.undefined;
+        done();
+      });
+    });
+
+    it('should output json as objects', (done) => {
+      var obj1;
+      var obj2;
+      var obj3;
+      var obj4;
+
+      reader = util.reader.new('json', fixturesDir + 'example.json', '!*');
+
+      reader.on('data', data => {
+        if (!obj1) obj1 = data;
+        else if (!obj2) obj2 = data;
+        else if (!obj3) obj3 = data;
+        else obj4 = data;
+      });
+
+      reader.on('end', () => {
+        expect(obj1).to.deep.equal({
+          cap_number: "13-0001",
+          date_received: "2013-01-07T00:00:00Z",
+          dist_occurrence: "35",
+          general_cap_classification: "BAD",
+          summary: "Summary 1",
+          the_geom: null,
+          the_geom_webmercator: null,
+          cartodb_id: 1
+        });
+        expect(obj2).to.deep.equal({
+          cap_number: "13-0002",
+          date_received: "2013-01-09T00:00:00Z",
+          dist_occurrence: "35",
+          general_cap_classification: "GOOD",
+          summary: "Summary 2",
+          the_geom: null,
+          the_geom_webmercator: null,
+          cartodb_id: 2
+        });
+        expect(obj3).to.deep.equal({
+          cap_number: "13-0003",
+          date_received: "2013-02-07T00:00:00Z",
+          dist_occurrence: "35",
+          general_cap_classification: "SOSO",
+          summary: "Summary 3",
+          the_geom: null,
+          the_geom_webmercator: null,
+          cartodb_id: 3
+        });
+        expect(obj4).to.be.undefined;
+        done();
+      });
     });
   });
 
