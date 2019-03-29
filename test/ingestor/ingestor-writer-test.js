564d50342081b38e8ec0ca645a302f541226d883
Jason Cox <jasonwilburcox@gmail.com>
2019-03-29 21:09:06 +0000

    Setup writer tests

--- test/ingestor/ingestor-writer-test.js
+++ test/ingestor/ingestor-writer-test.js
@@ -1,8 +1,12 @@
 var assert = require('assert');
+var { expect } = require('chai');
+var sinon = require('sinon');
+
+var mockReadableStream = require('stream').Readable;
 
 var util = require('../../ingestor/ingest-util');
 
-describe('Writer', () => {
+describe.only('Writer', () => {
   describe('Init', () => {
     it('should exist', () => {
       assert.ok(util.writer);
@@ -13,31 +17,83 @@ describe('Writer', () => {
     });
   });
 
-  xdescribe('Params', () => {
-    xit('should handle processedio', () => {
-      assert.fail('not implemented');
+  describe.only('IO', () => {
+    var input;
+    var writer;
+    var line1;
+    var line2;
+    var line3;
+    var line4;
+    var line5;
+
+    beforeEach(() => {
+      line1 = undefined;
+      line2 = undefined;
+      line3 = undefined;
+      line4 = undefined;
+      line5 = undefined;
+
+      input = new mockReadableStream({
+        objectMode: true,
+        read() {}
+      });
+
+      input.push({
+        id: 1,
+        data: "Data 1"
+      });
+      input.push({
+        id: 2,
+        data: "Data 2"
+      });
+      input.push({
+        id: 3,
+        data: "Data 3"
+      });
+      input.push(null);
     });
 
-    xit('should handle output', () => {
-      assert.fail('not implemented');
-    });
-  });
+    it('should output pp', (done) => {
+      writer = util.writer.new(input, 'pp');
 
-  xdescribe('IO', () => {
-    xit('should output pp', () => {
-      assert.fail('not implemented');
+      writer.on('data', data => {
+        if (!line1) line1 = data;
+        else if (!line2) line2 = data;
+        else if (!line3) line3 = data;
+        else if (!line4) line4 = data;
+        else line5 = data;
+      });
+
+      writer.on('end', () => {
+        expect(line1).to.equal('Hello');
+        expect(line2).to.equal('Hello');
+        expect(line3).to.equal('Hello');
+        expect(line4).to.equal('Hello');
+        expect(line5).to.equal('Hello');
+        done();
+      });
     });
 
-    xit('should output JSON', () => {
-      assert.fail('not implemented');
+    it('should output JSON', (done) => {
+      writer = util.writer.new(input, 'json');
+
+      expect(line1).to.equal('Hello');
+      expect(line2).to.equal('Hello');
+      expect(line3).to.equal('Hello');
+      expect(line4).to.equal('Hello');
+      expect(line5).to.equal('Hello');
+      done();
     });
 
-    xit('should output CSV', () => {
-      assert.fail('not implemented');
-    });
+    it('should output CSV', (done) => {
+      writer = util.writer.new(input, 'csv');
 
-    xit('should handle upload to Neo', () => {
-      assert.fail('not implemented');
+      expect(line1).to.equal('Hello');
+      expect(line2).to.equal('Hello');
+      expect(line3).to.equal('Hello');
+      expect(line4).to.equal('Hello');
+      expect(line5).to.equal('Hello');
+      done();
     });
   });
 });
