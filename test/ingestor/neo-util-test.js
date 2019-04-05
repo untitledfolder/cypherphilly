d404e8c36f0d2136044414b2cdc61247d4bb0f4b
Jason Cox <jasonwilburcox@gmail.com>
2019-04-05 00:41:48 +0000

    Setting up tests for Neo query generation

---
+++ test/ingestor/neo-util-test.js
@@ -0,0 +1,71 @@
+const assert = require('assert');
+
+describe.only('Neo Util', () => {
+  describe.only('Generate Labels', () => {
+    it('takes variable name', () => {
+      assert.fail('Not implemented');
+    });
+
+    it('takes no variable name', () => {
+      assert.fail('Not implemented');
+    });
+
+    it('handles no labels', () => {
+      assert.fail('Not implemented');
+    });
+
+    it('handles one label', () => {
+      assert.fail('Not implemented');
+    });
+
+    it('handles multiple labels', () => {
+      assert.fail('Not implemented');
+    });
+  });
+
+  describe('Generate matcher', () => {
+    it('uses one field', () => {
+      assert.fail('Not implemented');
+    });
+
+    it('uses multiple fields', () => {
+      assert.fail('Not implemented');
+    });
+  });
+
+  describe('Generate set map', () => {
+    it('maps out a map', () => {
+      assert.fail('Not implemented');
+    });
+  });
+
+  describe('Generate MATCH', () => {
+    it('uses the generated label and matcher', () => {
+      assert.fail('Not implemented');
+    });
+  });
+
+  describe('Generate CREATE', () => {
+    it('uses the generated label and map', () => {
+      assert.fail('Not implemented');
+    });
+  });
+
+  describe('Generate UPDATE', () => {
+    it('uses the generated MATCH and map', () => {
+      assert.fail('Not implemented');
+    });
+  });
+
+  describe('Generate MERGE', () => {
+    it('uses the generated label, matcher, and map', () => {
+      assert.fail('Not implemented');
+    });
+  });
+
+  describe('Generate create_or_update', () => {
+    it('uses the appropriate pattern', () => {
+      assert.fail('Not implemented');
+    });
+  });
+});
