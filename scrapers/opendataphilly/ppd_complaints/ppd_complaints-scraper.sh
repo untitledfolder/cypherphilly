322c19df503579c9f228fac22cfeadc186b8b819
Jason Cox <jasonwilburcox@gmail.com>
2019-06-06 18:07:44 +0000

    Removing the old way of doing things

--- scrapers/opendataphilly/ppd_complaints/ppd_complaints-scraper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-jq -c '.rows | .[] | {cartodb_id: .cartodb_id, the_geom: .the_geom, the_geom_webmercator: .the_geom_webmercator, cap_number: .cap_number, date_received: .date_received, dist_occurrence: .dist_occurrence, general_cap_classification: .general_cap_classification, summary: .summary}'
