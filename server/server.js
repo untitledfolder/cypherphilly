cb0110b5f8afe8f23efa5707e6979921265b0df3
Jason Cox <jasonwilburcox@gmail.com>
2019-07-09 21:02:34 +0000

    Migrating server to use SQL instead

--- server/server.js
+++ server/server.js
@@ -4,15 +4,27 @@ const datasetsRoot = '../datasets';
 const cypherUtil = require('../utils/cypher-util');
 const neo4j = require("neo4j-driver").v1;
 const neoConfig = require("../neo-config.json");
+const sqlConfig = require("../sql-config");
 const { Readable } = require('stream');
-
 var port = 7000;
+/*
 var neoDriver = neo4j.driver(
   neoConfig.host,
   neo4j.auth.basic(neoConfig.user, neoConfig.password),
   { disableLosslessIntegers: true }
 );
 var neoSession = neoDriver.session();
+*/
+
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: sqlConfig.host,
+    user: sqlConfig.user,
+    password: sqlConfig.password,
+    database: sqlConfig.database
+  }
+});
 
 var args = process.argv.slice(2);
 if (args.length) {
@@ -45,32 +57,57 @@ console.log("Enabled Datasets:");
 var apiList = [];
 enabledDatasets.forEach(dataset => {
   console.log(dataset.key);
-  apiList.push({
-    url: `/data/${dataset.key}`,
-    name: dataset.config.name,
-    labels: [dataset.config.label],
-    cypher: cypherUtil.genGetAll([dataset.config.label], limit)
-  });
 
   if (dataset.config.datasets) {
     dataset.config.datasets.forEach(subdataset => {
       console.log(' ', subdataset.key);
       apiList.push({
-        url: `/data/${dataset.key}/${subdataset.key}`,
+        url: `/api/data/${dataset.key}/${subdataset.key}`,
         name: `${dataset.config.name} - ${subdataset.name}`,
         labels: [dataset.config.label, subdataset.label],
-        cypher: cypherUtil.genGetAll([dataset.config.label, subdataset.label], limit)
+        cypher: cypherUtil.genGetAll([dataset.config.label, subdataset.label], limit),
+        table: subdataset.table
       });
+    });
+  }
+  else {
+    apiList.push({
+      url: `/api/data/${dataset.key}`,
+      name: dataset.config.name,
+      labels: [dataset.config.label],
+      cypher: cypherUtil.genGetAll([dataset.config.label], limit),
+      table: dataset.config.table
     });
   }
 });
 
 generateAPIs(app, apiList);
 
-app.get('/data', (req, res) => {
+app.get('/api/data', (req, res) => {
+  let apiContentStream = new Readable({read() {}});
   res.set('Content-Type', 'text/html');
-  res.send(generateAPIContentsView(apiList));
+  apiContentStream.pipe(res);
+
+  apiContentStream.push("<html><head><title>CypherPhilly: API List and Details</title></head>");
+  apiContentStream.push("<body>");
+  apiContentStream.push("<h1>API LIST<h1>");
 });
+
+function addApiDetailsToContent(contentStream, apiList) {
+  if (!apiList.length) {
+    contentStream.push("</body>");
+    contentStream.push(null);
+  }
+
+  let apiItem = apiList.shift();
+  contentStream.push(`<br><a href="${apiItem.url}">${apiItem.name}</a>`);
+  knex(apiItem.table).count('*').then(count => {
+    contentStream.push(`<br><p>Total: ${count}</p>`);
+    contentStream.push(`<br>
+    contentStream
+
+    return 
+}
 
 function generateAPIs(app, apiConfigs) {
   apiConfigs.forEach(apiConfig => {
@@ -82,6 +119,7 @@ function generateAPIs(app, apiConfigs) {
       responseStream.push('[');
       var skippedFirstComma = false;
 
+      /*
       neoSession.run(apiConfig.cypher)
       .subscribe({
         onNext: record => {
@@ -100,19 +138,27 @@ function generateAPIs(app, apiConfigs) {
           next(err);
         }
       });
+      */
+      knex.select().from(apiConfig.table).limit(limit)
+      .stream(stream => {
+        stream.on('data', data => {
+          console.log("Data:", data);
+          if (skippedFirstComma) responseStream.push(',\n');
+          else skippedFirstComma = true;
+
+          responseStream.push(JSON.stringify(data));
+        });
+        stream.on('end', () => {
+          responseStream.push(']');
+          responseStream.push(null);
+        });
+      }).then(() => {
+        console.log('done');
+      }).catch(
+        err => console.log(`Error reading data from ${apiConfig.table}: ${err}`)
+      );
     });
   });
-}
-
-function generateAPIContentsView(apiList) {
-  var returnView = "APIs:\n";
-
-  apiList.forEach(apiItem => {
-    returnView += `<br><a href="/api${apiItem.url}">${apiItem.name}</a>\n`;
-  });
-
-  console.log(returnView);
-  return returnView;
 }
 
 app.listen(port, () => {
