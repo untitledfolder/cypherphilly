b68f604188ca1380908226e1439506cea221bbe2
Jason Cox <jasonwilburcox@gmail.com>
2019-07-09 21:00:04 +0000

    Setup for new Data Table code

---
+++ src/app/data/data.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataComponent } from './data.component';
+
+@NgModule({
+  declarations: [DataComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DataModule { }
